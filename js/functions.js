bc47687324ba4ec5a298464642af1fdeb3b8e6d9
ART89-prog <dykpgilkom@mail.ru>
2022-09-07 00:40:08 +0300

    Сделал

--- js/functions.js
+++ js/functions.js
@@ -3,54 +3,68 @@ $(() => {
 	if (!is_touch_device() || !/(Mac|iPhone|iPod|iPad)/i.test(navigator.platform)) $('html').addClass('custom_scroll')
 
 
+	// Ленивая загрузка
+	setTimeout(() => {
+		observer = lozad('.lozad', {
+			rootMargin: '200px 0px',
+			threshold: 0,
+			loaded: (el) => el.classList.add('loaded')
+		})
+
+		observer.observe()
+	}, 200)
+
 
 	// Установка ширины стандартного скроллбара
 	$(':root').css('--scroll_width', widthScroll() + 'px')
 
+	
+
+	// Моб. версия
+	fiestResize = false
+
+	if ($(window).width() < 360) {
+		$('meta[name=viewport]').attr('content', 'width=360, user-scalable=no')
+
+		fiestResize = true
+	}
 
 
-	// Табы
-	// var locationHash = window.location.hash
+	if (is_touch_device()) {
+		// Закрытие моб. меню свайпом справо на лево
+		let ts
 
-	// $('body').on('click', '.tabs button', function (e) {
-	// 	e.preventDefault()
+		$('body').on('touchstart', (e) => { ts = e.originalEvent.touches[0].clientX })
 
-	// 	if (!$(this).hasClass('active')) {
-	// 		const $parent = $(this).closest('.tabs_container'),
-	// 			activeTab = $(this).data('content'),
-	// 			$activeTabContent = $(activeTab),
-	// 			level = $(this).data('level')
+		$('body').on('touchend', (e) => {
+			let te = e.originalEvent.changedTouches[0].clientX
 
-	// 		$parent.find('.tabs:first button').removeClass('active')
-	// 		$parent.find('.tab_content.' + level).removeClass('active')
-
-	// 		$(this).addClass('active')
-	// 		$activeTabContent.addClass('active')
-	// 	}
-	// })
-
-	// if (locationHash && $('.tabs_container').length) {
-	// 	const $activeTab = $('.tabs button[data-content=' + locationHash + ']'),
-	// 		$activeTabContent = $(locationHash),
-	// 		$parent = $activeTab.closest('.tabs_container'),
-	// 		level = $activeTab.data('level')
-
-	// 	$parent.find('.tabs:first button').removeClass('active')
-	// 	$parent.find('.tab_content.' + level).removeClass('active')
-
-	// 	$activeTab.addClass('active')
-	// 	$activeTabContent.addClass('active')
-
-	// 	$('html, body').stop().animate({ scrollTop: $activeTabContent.offset().top }, 1000)
-	// }
-
-
-
+			if ($('body').hasClass('menu_open') && ts > te + 50) {
+				// Свайп справо на лево
+				$('header .mob_menu_btn').removeClass('active')
+				$('body').removeClass('menu_open')
+				$('header .menu').removeClass('show')
+				$('.overlay').fadeOut(300)
+			} else if (ts < te - 50) {
+				// Свайп слева на право
+			}
+		})
+	}
 })
 
 
 
+$(window).resize(() => {
+	// Моб. версия
+	if (!fiestResize) {
+		$('meta[name=viewport]').attr('content', 'width=device-width, initial-scale=1, maximum-scale=1')
+		if ($(window).width() < 360) $('meta[name=viewport]').attr('content', 'width=360, user-scalable=no')
 
+		fiestResize = true
+	} else {
+		fiestResize = false
+	}
+})
 
 
 
